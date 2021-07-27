29de3e5b358d70de70f825198f204001697a8649
TeodoraGhiseOSF <83949742+GhiseTeodora@users.noreply.github.com>
2021-07-27 21:58:17 +0300

    task1

---
+++ Task1/force-app/main/default/aura/accountsearch/accountsearchController.js
@@ -0,0 +1,30 @@
+({
+    doInit: function(component, event, helper) {
+        helper.getAccountList(component);
+    },
+    searchKeyChange: function(component, event) {
+        var searchKey = component.find("searchKey").get("v.value");
+        var action = component.get("c.findByEmail");
+        action.setParams({
+            "searchKey": searchKey
+        });
+        action.setCallback(this, function(a) {
+            component.set("v.accounts", a.getReturnValue());
+        });
+        $A.enqueueAction(action);
+    },
+    
+    downlandCSV: function(component, event, helper){
+        var stockData = component.get("v.accounts");
+        var csv = helper.convertToCSV(component,stockData);
+        if(csv == null){
+            return;
+        }
+          var hiddenElement = document.createElement('a');
+          hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+          hiddenElement.download = 'ExportAccount.csv'; 
+          document.body.appendChild(hiddenElement); 
+    	  hiddenElement.click(); 
+        }, 
+    
+})
---
+++ Task1/force-app/main/default/aura/accountsearch/accountsearchHelper.js
@@ -0,0 +1,35 @@
+({      
+    getAccountList: function(component) {
+        var action = component.get('c.getAccounts');
+        var self = this;
+        action.setCallback(this, function(actionResult) {
+            component.set('v.accounts', actionResult.getReturnValue());
+        });
+        $A.enqueueAction(action);
+    },
+    convertToCSV : function(component,objectRecords){
+        var csvStringResult, counter, keys, columnDivider, lineDivider;
+        
+        columnDivider = ',';
+        lineDivider =  '\n'; 
+        keys = ['Name','Email__c'];
+        
+        csvStringResult = '';
+        csvStringResult += keys.join(columnDivider);
+        csvStringResult += lineDivider;
+        
+        for(var i=0; i < objectRecords.length; i++){   
+            counter = 0;
+            for(var sTempkey in keys) {
+                var skey = keys[sTempkey] ;  
+                if(counter > 0){ 
+                    csvStringResult += columnDivider; 
+                }   
+                csvStringResult +=  objectRecords[i][skey]; 
+                counter++;   
+            }
+            csvStringResult += lineDivider;
+        }
+        return csvStringResult;        
+    },
+})
